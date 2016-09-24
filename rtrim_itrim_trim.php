94cafd05b122c6b7df29fdc31d7ddd9fd44538bc
Ajit Das <ajitdas2900@gmail.com>
2016-09-24 09:23:20 +0600

    substrcompare

--- rtrim_itrim_trim.php
+++ rtrim_itrim_trim.php
@@ -1,8 +1,14 @@
 <?php
 
 $hello  = "\t\tHello World";
+$test = "\nThis is BITM";
 
 var_dump( $hello);
+var_dump($test);
+echo $test;
 echo "<br>";
 $trimmed = rtrim($hello, "Hdle");
+$testtrim = trim($test);
+
 var_dump($trimmed);
+var_dump($testtrim);
---
+++ substr_compare.php
@@ -0,0 +1,5 @@
+<?php
+
+echo substr_compare("abcde", "bc", 2, 3).'<br>';
+
+echo substr_compare("charles", "ar", 1, 2);
